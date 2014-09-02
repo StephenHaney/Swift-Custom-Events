b8af144c0fe7f149d6396b13f93651f38da2c296
Trevor Hartman <trevorhartman@gmail.com>
2014-09-02 07:43:02 -0600

    Pass `information` to event listeners
    The type [Any] allows you to pass Swift `structs` and other value types.

--- Events.swift
+++ Events.swift
@@ -10,13 +10,13 @@ import Foundation
 class EventManager {
     // using NSMutableArray as Swift arrays can't change size inside dictionaries (yet, probably)
     var listeners = Dictionary<String, NSMutableArray>();
-    
+
     // Create a new event listener
     // + eventName: Matching trigger eventNames will cause this listener to fire
     // + action: The block of code you want executed when the event triggers
-    func listenTo(eventName:String, action:(()->())) {
+    func listenTo(eventName:String, action:(([Any])->())) {
         let newListenerAction = EventListenerAction(action);
-        
+
         if let listenerArray = self.listeners[eventName] {
             // action array exists for this event, add new action to it
             listenerArray.addObject(newListenerAction);
@@ -26,13 +26,13 @@ class EventManager {
             self.listeners[eventName] = [newListenerAction] as NSMutableArray;
         }
     }
-    
+
     // Removes all listeners by default, or specific listeners through paramters
     // + eventName: If an event name is passed, only listeners for that event will be removed
     func removeListeners(eventNameToRemoveOrNil:String?) {
         if let eventNameToRemove = eventNameToRemoveOrNil {
             // remove listeners for a specific event
-            
+
             if let actionArray = self.listeners[eventNameToRemove] {
                 // actions for this event exist
                 actionArray.removeAllObjects();
@@ -43,15 +43,15 @@ class EventManager {
             self.listeners.removeAll(keepCapacity: false);
         }
     }
-    
+
     // Triggers an event
     // + eventName: Matching listener eventNames will fire when this is called
     // + information: pass values to your listeners
-    func trigger(eventName:String, information:AnyObject[] = []) {
+    func trigger(eventName:String, information:[Any] = []) {
         if let actions = self.listeners[eventName] {
             for actionToPerform in actions {
                 if actionToPerform is EventListenerAction {
-                    (actionToPerform as EventListenerAction).action();
+                    (actionToPerform as EventListenerAction).action(information);
                 }
             }
         }
@@ -60,9 +60,9 @@ class EventManager {
 
 // Class to hold actions to live in NSMutableArray
 class EventListenerAction {
-    let action:(() -> ());
-    
-    init(callback:(() -> ())) {
+    let action:(([Any]) -> ());
+
+    init(callback:(([Any]) -> ())) {
         self.action = callback;
     }
 }
